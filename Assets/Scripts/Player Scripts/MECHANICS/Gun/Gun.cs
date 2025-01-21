ea62c759e577204d350cb4702030d9fa8e126404
Ninten7 <90481301+Nintenerd7@users.noreply.github.com>
2025-01-21 13:15:28 +0000

    Repository has been added

---
+++ Assets/Scripts/Player Scripts/MECHANICS/Gun/Gun.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+   public float damage = 10f;
+   public float range = 100f;
+   public ParticleSystem Muzzleflash;
+
+   public Camera fpsCam;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+          Shoot_Gun();
+        }
+    }
+    public void Shoot_Gun()
+    {
+      Muzzleflash.Play();//Initiate Particle
+      RaycastHit hit;
+      if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+      {
+        Debug.Log(hit.transform.name);
+        Target target = hit.transform.GetComponent<Target>();//gets target reference from target script
+
+        if(target != null) target.TakeDamage(damage);
+      }
+    }
+}
---
+++ Assets/Scripts/Player Scripts/MOVEMENT/Look.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Look : MonoBehaviour
+{
+    //LOOK SCRIPT: Calculates the camera movement when moving the mouse in first person
+    public float mouseSense = 100f;//tracks mouse sensitivity 
+    public Transform playerBody;//tracks player movement 
+
+
+    float X_Rotation;//calculate the horizontal rotation of the camera
+
+    //[SerializeField]public  PauseMenu PauseGame;
+    void Start()
+    {
+            Cursor.lockState = CursorLockMode.Locked;//locks cursor onto game screen
+            Cursor.visible = false; //cursor is not visible
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveCamera();
+       // if (PauseGame.IsPaused)//if the game is paused 
+      //  {
+
+           // Cursor.lockState = CursorLockMode.None;//unlock cursor
+           // Cursor.visible = true;//mouse is visible
+           
+      //  }
+
+    }
+
+    public void MoveCamera()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSense * Time.deltaTime;//allows the player to control the camera sideways 
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSense * Time.deltaTime;//allows the player to control the camera Upwards
+        //calculation for X and Y camera rotation
+        X_Rotation -= mouseY;
+        X_Rotation = Mathf.Clamp(X_Rotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(X_Rotation, 0f, 0f);
+
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+
+}
