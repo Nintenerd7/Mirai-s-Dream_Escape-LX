b595c58064a55c4847f45cec7704aaec20de9a78
brandonfox <brandonfox@hotmail.co.uk>
2025-01-22 19:50:56 +0700

    Added basic spike trap

---
+++ Assets/Scripts/Traps/SpikeTrap.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SpikeTrap : MonoBehaviour
+{
+    private bool _spikeTriggered = false;
+    private bool _alreadyHitPlayer = false;
+
+    [Tooltip("The distance that the spikes will travel on trigger")]
+    public float SpikeDistance = 1;
+    public float SpikeTimeToExtend = 0.4f;
+    public float SpikeTimeFullyExtended = 1;
+    public float RetractionTime = 3;
+
+    // TODO: Add some data here for collecting the player script
+    public UnityEvent OnHitPlayer;
+
+    public TrapController trapController;
+
+    public void TriggerSpikes() {
+        if (_spikeTriggered) return;
+        _spikeTriggered = true;
+        StartCoroutine(ExtendSpikes(SpikeTimeToExtend));
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player") && !_alreadyHitPlayer && _spikeTriggered) {
+            Debug.Log("Hit Player");
+            _alreadyHitPlayer = true;
+            OnHitPlayer.Invoke();
+        }
+    }
+
+    private IEnumerator ExtendSpikes(float timeToExtend) {
+        float elapsedTime = 0;
+        Vector3 originalPosition = transform.position;
+        Vector3 targetPosition = transform.position + transform.up * SpikeDistance;
+        while (elapsedTime < timeToExtend) {
+            transform.position = Vector3.Lerp(originalPosition, targetPosition, elapsedTime / timeToExtend);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        transform.position = targetPosition;
+        yield return new WaitForSeconds(SpikeTimeFullyExtended);
+        StartCoroutine(RetractSpikes(RetractionTime));
+    }
+
+    private IEnumerator RetractSpikes(float timeToRetract) {
+        yield return new WaitForSeconds(SpikeTimeFullyExtended);
+        float elapsedTime = 0;
+        Vector3 originalPosition = transform.position;
+        Vector3 targetPosition = transform.position - transform.up * SpikeDistance;
+        while (elapsedTime < timeToRetract) {
+            transform.position = Vector3.Lerp(originalPosition, targetPosition, elapsedTime / timeToRetract);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        transform.position = targetPosition;
+        
+        ResetState();
+    }
+
+    private void ResetState() {
+        _alreadyHitPlayer = false;
+        _spikeTriggered = false;
+
+        if (trapController != null)
+        {
+            trapController.ResetTrap();
+        }
+    }
+}
