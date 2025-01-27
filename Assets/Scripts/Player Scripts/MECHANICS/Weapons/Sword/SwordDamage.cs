fec33ff9e7c487a71853c3e3f1f9b167c0c15349
Ninten7 <90481301+Nintenerd7@users.noreply.github.com>
2025-01-27 06:05:06 +0000

    Sword swing is implimented (Wip)

--- Assets/Scripts/Player Scripts/MECHANICS/Weapons/Sword/SwordDamage.cs
+++ Assets/Scripts/Player Scripts/MECHANICS/Weapons/Sword/SwordDamage.cs
@@ -6,8 +6,6 @@ public class SwordDamage : MonoBehaviour
 {
   
     public Weapon_Logic Hitbox;
-    public GameObject BloodParticle;
-    public GameObject solidObject;
 
     public float damage = 10f;
     //
@@ -17,11 +15,10 @@ public class SwordDamage : MonoBehaviour
         if(other.tag == "Enemy"&& Hitbox.DoingDamage)
         {
         Target target = other.GetComponent<Target>();//gets target object reference from target script without being public
-
+        
         if(target != null) target.TakeDamage(damage);// if target is not empty then the enemy will be damaged using the damage number above to pass the parameter
             Debug.Log("you hit a foe");
-            other.GetComponent<Animator>().SetTrigger("Hit");//hit trigger activates
-            Instantiate(BloodParticle, new Vector3(other.transform.position.x, transform.position.y, other.transform.position.z), other.transform.rotation);
+            other.GetComponent<Animator>().SetTrigger("Attack");//hit trigger activates
         }
        
     }
