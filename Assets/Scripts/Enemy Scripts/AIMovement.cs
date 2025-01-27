909b7bb22a0c6a9579465f96bed7a3740466b0aa
Ninten7 <90481301+Nintenerd7@users.noreply.github.com>
2025-01-27 05:27:22 +0000

    Enemy AI Prototype is up an running

---
+++ Assets/Scripts/Enemy Scripts/AIMovement.cs
@@ -0,0 +1,107 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class AIMovement : MonoBehaviour
+{
+  
+
+    public NavMeshAgent _agent;
+
+    public Transform playerTarget;
+
+    public LayerMask IsGround, IsPlayer;
+
+    //patrolling
+    public Vector3 WalkPoint;
+    bool walkpointSet;
+    public float range;
+    //
+
+    //attacking 
+    public float AttackCoolDown;
+    bool HasAttacked;
+    //
+
+    //states
+    public float ViewRange, AttackRange;
+    public bool PlayerInView, AttackPlayer;
+    //
+     public PlayerHearts damage;
+    //
+
+    //AWAKE
+    private void Awake()
+    {
+        playerTarget = GameObject.Find("MIRAI").transform;//this is how the player gets tracked
+        _agent = GetComponent<NavMeshAgent>();
+    }
+    //
+
+    //PRIVATE UPDATE
+    private void Update()
+    {
+        //check if player is in range
+        PlayerInView = Physics.CheckSphere(transform.position, ViewRange, IsPlayer );
+        //
+        AttackPlayer = Physics.CheckSphere(transform.position, AttackRange, IsPlayer);
+
+        if (!PlayerInView && !AttackPlayer) Patrolling();//calls patrolling 
+        if (PlayerInView && !AttackPlayer) ChaseTarget();//calls chase  
+
+    }
+    //
+
+
+
+    //patrolling
+    private void Patrolling()
+    {
+        if(!walkpointSet)FindWalkPoint();//calls find walk point
+        if (walkpointSet)
+        {
+            _agent.SetDestination(WalkPoint);
+        }
+
+        Vector2 distanceToWalkPoint = transform.position - WalkPoint;
+
+        //walkpoint reached
+        if (distanceToWalkPoint.magnitude < 1f)
+        {
+            walkpointSet = false;
+        }
+        //
+    }
+
+    //calculate walk point
+    private void FindWalkPoint()
+    {
+        float randomZ = Random.Range(-range, range);//calculates movement on Z axis 
+        float randomX = Random.Range(-range, range);//calculates movement on X axis
+
+        WalkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);//
+
+        if (Physics.Raycast(WalkPoint, -transform.up, 2f, IsGround))
+        {
+            walkpointSet = true;
+        }
+      
+    }
+
+    //
+    //Chase
+    private void ChaseTarget()
+    {
+        _agent.SetDestination(playerTarget.position);
+    }
+    //
+    private void OnTriggerEnter(Collider other)//box collider that detects the player in their personal space
+    {
+        if (other.tag == "Player")//if the box detects the player 
+        {
+          //Play Hurt Animation
+          damage.TakeDamage(1);//takes one heart 
+        }
+    }
+
+}
